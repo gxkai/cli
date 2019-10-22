3b7750c995f7e9336317a840b95ab45a063dc5c9
yangming <472102644@qq.com>
2019-10-22 19:19:43 +0800

    fix:origin

--- src/load.js
+++ src/load.js
@@ -4,7 +4,7 @@ if (!window._MICRO_APP_CONFIG || !window._MICRO_APP_CONFIG.current) {
 
 window._MICRO_APP_CONFIG.appendAssets(
   {{ projectName }}
-  window._MICRO_APP_CONFIG.origin,
+  window._MICRO_APP_CONFIG.current.origin,
   {{ assetsArr }}
 )
 
