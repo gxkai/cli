7751f43f86e04dbdeb1b6585e3a987d3e31fae13
yangming <472102644@qq.com>
2019-10-22 11:33:26 +0800

    fix: print

--- src/load.js
+++ src/load.js
@@ -2,9 +2,8 @@ if (!window._MICRO_APP_CONFIG || !window._MICRO_APP_CONFIG.current) {
   throw new Error('_MICRO_APP_CONFIG or config.current is not exist!')
 }
 
-var dev = location.port !== '' && location.port !== '80' && location.port !== '443'
 window._MICRO_APP_CONFIG.appendAssets(
-  dev ? window._MICRO_APP_CONFIG.localOrigin : window._MICRO_APP_CONFIG.origin,
+  window._MICRO_APP_CONFIG.origin,
   {{ assetsArr }}
 )
 
