02f28b6906fac60b323f5053fd7eadefdeaddb5c
yangming <472102644@qq.com>
2019-10-22 11:55:08 +0800

    fix: basename

--- src/load.js
+++ src/load.js
@@ -3,6 +3,7 @@ if (!window._MICRO_APP_CONFIG || !window._MICRO_APP_CONFIG.current) {
 }
 
 window._MICRO_APP_CONFIG.appendAssets(
+  {{ projectName }}
   window._MICRO_APP_CONFIG.origin,
   {{ assetsArr }}
 )
