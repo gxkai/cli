5ff369900295514c02380e8d35aad787eff6534b
yangming <472102644@qq.com>
2020-03-19 10:49:00 +0800

    feat: child from router

--- src/load.js
+++ src/load.js
@@ -2,6 +2,11 @@ if (!window._MICRO_APP_CONFIG || !window._MICRO_APP_CONFIG.current) {
   throw new Error('_MICRO_APP_CONFIG or config.current is not exist!')
 }
 
+if ({{ routerFromChild }}) {
+  // window._MICRO_APP_CONFIG_MENU
+  window._MICRO_APP_CONFIG.current.child = {{ routerFromChildContent }}
+}
+
 window._MICRO_APP_CONFIG.appendAssets(
   {{ projectName }}
   window._MICRO_APP_CONFIG.current.dir,
